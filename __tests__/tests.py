a66b7ba11ec9326300d5f8a05056b3aee6adfcdb
DPLATA <diegogplata@gmail.com>
2021-07-10 17:43:42 -0500

    add dependency, first passing test post route

--- __tests__/tests.py
+++ __tests__/tests.py
@@ -1,5 +1,11 @@
 import requests
+from requests.models import Response
 
-def test_get_employee_details_check_status_code_equals_200():
-     response = requests.get('http://127.0.0.1:5000/')
+# def test_get_employee_details_check_status_code_equals_200():
+     # response = requests.get('http://127.0.0.1:5000/')
+     # assert response.status_code == 200
+
+def test_post_user_check_response_ok():
+     data = {'email':'test@test.com','password':'something', 'name':'test_name'}
+     response = requests.post('http://127.0.0.1:5000/users', data=data)
      assert response.status_code == 200
