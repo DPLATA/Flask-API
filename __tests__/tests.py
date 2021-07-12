9cff735d119c96175c4f465c8f21210415954115
DPLATA <diegogplata@gmail.com>
2021-07-11 22:23:10 -0500

    update route with passing test

--- __tests__/tests.py
+++ __tests__/tests.py
@@ -4,7 +4,7 @@ import random
 import string
 
 def test_get_all_users_list_check_status_code_equals_200():
-     response = requests.get('http://127.0.0.1:5000/users')
+     response = requests.get('http://127.0.0.1:5000/get/users')
      
      # TODO: remove prints
      j = json.loads(response.text)
@@ -13,7 +13,7 @@ def test_get_all_users_list_check_status_code_equals_200():
      assert response.status_code == 200
 
 def test_get_single_user_detail_check_status_code_equals_200():
-     response = requests.get('http://127.0.0.1:5000/users/1')
+     response = requests.get('http://127.0.0.1:5000/get/users/1')
      
      # TODO: remove prints
      j = json.loads(response.text)
@@ -30,7 +30,7 @@ def test_post_user_check_status_code_equals_200():
      letters = string.ascii_lowercase
      random_username = ''.join(random.choice(letters) for i in range(10))
      data = {'email':random_username+'@testing.com', 'password':'somethingtest', 'name':'test_name4'}
-     response = requests.post('http://127.0.0.1:5000/users', data=json.dumps(data), headers=headers)
+     response = requests.post('http://127.0.0.1:5000/post/users', data=json.dumps(data), headers=headers)
      
      # TODO: remove prints
      j = json.loads(response.text)
@@ -47,7 +47,24 @@ def test_post_address_check_status_code_equals_200():
         'Accept': mimetype
      }
      data = {'zip_code':'04350', 'municipality':'my_municipality', 'city':'sin_city', 'state':'quaker', 'country':'MX'}
-     response = requests.post('http://127.0.0.1:5000/users/1/addresses', data=json.dumps(data), headers=headers)
+     response = requests.post('http://127.0.0.1:5000/post/users/2/addresses', data=json.dumps(data), headers=headers)
+     
+     # TODO: remove prints
+     j = json.loads(response.text)
+     print(j)
+
+     # TODO: assert response text vs. data sent
+     
+     assert response.status_code == 200
+     
+def test_put_to_update_address_check_status_code_equals_200():
+     mimetype = 'application/json'
+     headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+     }
+     data = {'zip_code':'04370', 'municipality':'my_municipality_updated', 'city':'sin_city', 'state':'quaker', 'country':'MX'}
+     response = requests.put('http://127.0.0.1:5000/update/users/1/addresses', data=json.dumps(data), headers=headers)
      
      # TODO: remove prints
      j = json.loads(response.text)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import request, jsonify
 from app import app, database
-from app.models import User, users_schema, user_schema, Address
+from app.models import User, users_schema, user_schema, Address, address_schema, addresses_schema
 
-@app.route('/users', methods=['POST'])
+@app.route('/post/users', methods=['POST'])
 def create_user():
     email = request.json['email']
     password = request.json['password']
@@ -17,18 +17,18 @@ def create_user():
     return user_schema.jsonify(new_user)
 
 
-@app.route('/users', methods=['GET'])
+@app.route('/get/users', methods=['GET'])
 def get_users():
     all_users = User.query.all()
     # list_of_all_users = users_schema.dump(all_users)
     return user_schema.jsonify(all_users)
 
-@app.route('/users/<id>', methods=['GET'])
+@app.route('/get/users/<id>', methods=['GET'])
 def get_user(id):
     user = User.query.get(id)
     return user_schema.jsonify(user)
 
-@app.route('/users/<id>/addresses', methods=['POST'])
+@app.route('/post/users/<id>/addresses', methods=['POST'])
 def create_address(id):
     zip_code = request.json['zip_code']
     municipality = request.json['municipality']
@@ -43,4 +43,25 @@ def create_address(id):
     database.session.add(new_address)
     database.session.commit()
     
-    return user_schema.jsonify(new_address)
+    return address_schema.jsonify(new_address)
+
+@app.route('/update/users/<id>/addresses', methods=['PUT'])
+def update_address(id):
+    address = Address.query.get(id)
+    
+    if address is None:
+        return jsonify({'message': 'Address not found'}), 404
+
+    address.zip_code = request.json['zip_code']
+    address.municipality = request.json['municipality']
+    address.city = request.json['city']
+    address.state = request.json['state']
+    address.country = request.json['country']
+    address.user_id = id
+
+    # new_address = Address(zip_code, municipality, city, state, country, user_id)
+    
+    database.session.merge(address)
+    database.session.commit()
+    
+    return address_schema.jsonify(address)
