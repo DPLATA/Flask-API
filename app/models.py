8bb2e7a308b8984e5bca8d2b8fcfba7253b658c5
DPLATA <diegogplata@gmail.com>
2021-07-11 21:07:56 -0500

    add passing test for post address, update model with relationship, update post address route

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from app import database
 from app import marshmallow
+import datetime
 
 class Base(database.Model):
     __abstract__  = True
 
     id = database.Column(database.Integer, primary_key=True)
-    date_created  = database.Column(database.DateTime,  default=database.func.current_timestamp())
-    date_modified = database.Column(database.DateTime,  default=database.func.current_timestamp(), onupdate=database.func.current_timestamp())
+    date_created = database.Column(database.DateTime,  default=database.func.current_timestamp())
+    # date_modified = database.Column(database.DateTime,  default=database.func.current_timestamp(), onupdate=database.func.current_timestamp())
 
 class User(Base):
     __tablename__ = 'user'
@@ -19,34 +20,36 @@ class User(Base):
         self.email = email
         self.password = password
         self.name = name
+        self.date_created = datetime.datetime.now()
 
 
 class UserSchema(marshmallow.Schema):
     class Meta:
-        fields = ('id', 'email', 'password', 'name')
+        fields = ('id', 'email', 'password', 'name', 'date_created')
 
 class Address(Base):
     __tablename__ = 'address'
 
-    zip_code = database.Column(database.String(35), unique=True, nullable=False)
+    zip_code = database.Column(database.String(35), unique=False, nullable=False)
     municipality = database.Column(database.String(40), nullable=False)
     city = database.Column(database.String(60), nullable=False)
     state = database.Column(database.String(80))
     country = database.Column(database.String(20), nullable=False)
-    # user_id = database.Column(database.Integer, database.ForeignKey('user.id'))
-    # user = database.relationship('User', back_populates='address')
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'))
+    # TODO: backpopulate array of addresses user = database.relationship('User', back_populates='address')
 
-    def __init__(self, zip_code, municipality, city, state, country):
+    def __init__(self, zip_code, municipality, city, state, country, user_id):
         self.zip_code = zip_code
         self.municipality = municipality
         self.city = city
         self.state = state
         self.country = country
-        # TODO: init user_id for foreign key
+        self.date_created = datetime.datetime.now()
+        self.user_id = user_id
 
 class AddressSchema(marshmallow.Schema):
     class Meta:
-        fields = ('id', 'zip_code', 'municipality', 'city', 'state', 'country')
+        fields = ('id', 'zip_code', 'municipality', 'city', 'state', 'country', 'date_created', 'user_id')
         # fields = ('id', 'zip_code', 'municipality', 'city', 'state', 'country', 'user_id')
 
 database.create_all()
@@ -54,5 +57,5 @@ database.create_all()
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-address_schema = UserSchema()
-addresses_schema = UserSchema(many=True)
+address_schema = AddressSchema()
+addresses_schema = AddressSchema(many=True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from app import app, database
-from app.models import User, users_schema, user_schema
+from app.models import User, users_schema, user_schema, Address
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -8,6 +8,7 @@ def create_user():
     password = request.json['password']
     name = request.json['name']
 
+    # TODO: hash password
     new_user = User(email, password, name)
     
     database.session.add(new_user)
@@ -19,10 +20,27 @@ def create_user():
 @app.route('/users', methods=['GET'])
 def get_users():
     all_users = User.query.all()
-    list_of_all_users = users_schema.dump(all_users)
-    return jsonify(list_of_all_users)
+    # list_of_all_users = users_schema.dump(all_users)
+    return user_schema.jsonify(all_users)
 
 @app.route('/users/<id>', methods=['GET'])
 def get_user(id):
     user = User.query.get(id)
     return user_schema.jsonify(user)
+
+@app.route('/users/<id>/addresses', methods=['POST'])
+def create_address(id):
+    zip_code = request.json['zip_code']
+    municipality = request.json['municipality']
+    city = request.json['city']
+    state = request.json['state']
+    country = request.json['country']
+    user_id = id
+    # print(user_id)
+
+    new_address = Address(zip_code, municipality, city, state, country, user_id)
+    
+    database.session.add(new_address)
+    database.session.commit()
+    
+    return user_schema.jsonify(new_address)
